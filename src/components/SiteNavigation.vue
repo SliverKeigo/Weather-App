9c86c61f13fae401c32a186c86f3c88fe0793cd6
SliverKeigo <1226355793@qq.com>
2024-02-19 00:17:21 +0800

    feat: region search

---
+++ src/components/SiteNavigation.vue
@@ -0,0 +1,60 @@
+<template>
+    <header class="sticky top-0 bg-weather-primary shadow-lg">
+        <nav class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6">
+            <RouterLink :to="{ name: 'home' }">
+                <div class="flex items-center gap-3 ">
+                    <i class="fa-solid fa-sun text-2xl"></i>
+                    <p class="text-2xl">The Local Weather</p>
+                </div>
+            </RouterLink>
+
+            <div class="flex  gap-3 flex-1 justify-end">
+                <i class="fa-solid fa-circle-info text-xl hover:text-weather-secondary duration-150 cursor-pointer" @click="toggleModal"></i>
+                <i class="fa-solid fa-plus text-xl hover:text-weather-secondary duration-150 cursor-pointer"></i>
+            </div>
+
+            <BaseModal :modal-active="modalActive" @close-modal="toggleModal" >
+                <div class="text-black">
+                    <h1 class="text-2xl mb-1">关于:</h1>
+                    <p class="mb-4">
+                        The Local Weather 允许您查看所选城市的当前和未来天气。
+                    </p>
+                    <h2 class="text-2xl">如何使用:</h2>
+                    <ol class="list-decimal list-inside mb-4">
+                        <li>
+                            在搜索栏中输入城市名称进行搜索。
+                        </li>
+                        <li>
+                            在搜索结果中选择一个城市，即可查看所选城市的当前天气情况。
+                        </li>
+                        <li>
+                            点击右上角的 "+"图标跟踪城市。这将保存该城市，以便以后在主页上查看。
+                        </li>
+                    </ol>
+
+                    <h2 class="text-2xl">删除城市</h2>
+                    <p>
+                        如果您不想再跟踪某个城市，只需在主页上选择该城市。在页面底部会有一个删除城市的选项。
+                    </p>
+                </div>
+            </BaseModal>
+        </nav>
+
+    </header>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import BaseModal from './BaseModal.vue';
+import { ref } from 'vue';
+
+
+let modalActive = ref();
+
+function toggleModal(){
+    modalActive.value = !modalActive.value;
+}
+
+
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,74 @@
+<template>
+    <main class="container text-white">
+        <div class="pt-4 mb-8 relative">
+            <input v-model="searchQuery" @input="getSearchResults" type="text" placeholder="搜索城市" class="py-2 px-1 w-full bg-transparent border-b
+             focus:border-weather-secondary focus:outline-none
+            focus:shadow-[0px_1px_0_0_#004E71]" />
+            <ul v-if="searchResult" class="absolute bg-weather-secondary text-white w-full
+            shadow-md py-2 px-1 top-[66px]">
+            {{ searchResult.length }}
+                <p v-if="searchError">
+                    抱歉，出错了，请稍后重试。
+                </p>
+                <p v-if="Object.keys(searchResult).length === 0 && !searchError">
+                    没有符合您搜索条件的结果，请尝试其他搜索条件。
+                </p>
+                <template v-else>
+                    <li v-for=" result in searchResult" :key="searchResult.id" class="py-2 cursor-pointer">
+                        {{ result.name }}
+                    </li>
+                </template>
+
+            </ul>
+        </div>
+
+    </main>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue';
+import axios from 'axios';
+
+let searchQuery = ref("")
+let queryTimeout = ref()
+let Api = 'b0ba9afa21d34494a3f00e340cdbc259'
+let searchResult = ref()
+let searchError = ref()
+
+function getSearchResults() {
+    clearTimeout(queryTimeout.value)
+    searchError.value = false
+    try {
+        queryTimeout.value = setTimeout(async () => {
+            if (searchQuery.value.length > 0) {
+                try {
+                    let { data } = await axios.get(`https://geoapi.qweather.com/v2/city/lookup?location=${searchQuery.value}&key=${Api}`)
+                    if(data.location){
+                        searchResult.value = data.location
+                    }else{
+                        searchResult.value = {}
+                    }
+                    
+                    return;
+                }
+                catch (error) {
+                    searchError.value = true
+                    searchResult.value = {}
+                }
+
+
+                //     let { data: { daily } } = await axios.get(`https://devapi.qweather.com/v7/weather/now?location=${locationId}&key=${Api}`)
+                //     searchResult.value = daily
+
+            }
+            searchResult.value = null;
+        }, 300);
+    } catch (error) {
+        console.error('发生错误：', error);
+    }
+}
+
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
